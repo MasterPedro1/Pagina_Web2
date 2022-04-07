079d541504f7159fba1b966dd2bc05d3255b874b
MasterPedro1 <99909385+MasterPedro1@users.noreply.github.com>
2022-04-07 11:19:12 -0500

    Correcciones

--- Php/formulario.php
+++ Php/formulario.php
@@ -2,9 +2,12 @@
 
 $nombre = $_GET['nombre'];
 $edad = $_GET['edad'];
+$ocupa = $_Get['edad'];
 
 echo "<h2> Informacion recibida mediante php</h2>";
 echo "El nombre es: " . $nombre . "<br/>";
 echo "La edad es: " . $edad . "<br/>";
+echo "Su ocupación es: " . $ocupa . "<br/>";
+
 
 ?>
